7cfb91769fe70110abc30dbe4a3e0af7ff482043
vannur_swamy <varunsandeshkuruba@gmail.com>
2026-08-11 15:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (64.16%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,31 +1,30 @@
 class Solution {
     public int missingInteger(int[] nums) {
-        if(nums.length==1){
-            return nums[0]+1;
-        }
-        HashSet<Integer> set=new HashSet<>();
-        set.add(nums[0]);
-        int curr_sum=nums[0];
-        int i;
-        for(i=1;i<nums.length;i++){
-            set.add(nums[i]);
-            if(nums[i]==nums[i-1]+1){
-                curr_sum+=nums[i];
-            }else{
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
                 break;
             }
         }
-        for(int j=i;j<nums.length;j++){
-            set.add(nums[j]);
+
+        int answer = sum;
+
+        while (contains(nums, answer)) {
+            answer++;
         }
-        for(int j=curr_sum;j<=50;j++){
-            if(set.contains(j)){
-                curr_sum++;
-                continue;
-            }else{
-                break;
+
+        return answer;
+    }
+
+    private boolean contains(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
             }
         }
-        return curr_sum;
+        return false;
     }
 }
